e09dd95eed897d5538ebc70c40b4ea407a9bba26
Petar Slovic <petar@vxpass.com>
2024-02-27 18:46:14 +0100

    File upload - improve file support logic

--- src/utils/file.js
+++ src/utils/file.js
@@ -1,3 +1,8 @@
+/**
+ * Converts a file to base64 format.
+ * @param {File} file - The file to be converted.
+ * @returns {Promise<string>} - A promise that resolves with the base64 representation of the file.
+ */
 export const toB64 = (file) =>
   new Promise((resolve, reject) => {
     const reader = new FileReader();
@@ -6,8 +11,16 @@ export const toB64 = (file) =>
     reader.readAsDataURL(file);
   });
 
+/**
+ * The maximum file size limit for file uploads.
+ */
 export const UPLOAD_FILE_SIZE_LIMIT = 1024 * 50; // 50kb
 
+/**
+ * Formats the given number of bytes into a human-readable string.
+ * @param {number} bytes - The number of bytes.
+ * @returns {string} - The formatted string representing the file size.
+ */
 export function formatBytes(bytes) {
   if (bytes < 1024) {
     return bytes + "B";
@@ -18,6 +31,40 @@ export function formatBytes(bytes) {
   }
 }
 
+/**
+ * Checks if the given MIME type can be played by an audio or video element.
+ * @param {string} mimeType - The MIME type to check.
+ * @returns {boolean} - True if the MIME type can be played, false otherwise.
+ */
+function canPlayType(mimeType) {
+  if (!mimeType) {
+    return false;
+  }
+
+  const elementType = mimeType.startsWith("audio")
+    ? "audio"
+    : mimeType.startsWith("video")
+    ? "video"
+    : null;
+
+  if (!elementType) {
+    return false;
+  }
+
+  const dummyMediaElement = document.createElement(elementType);
+  if (!dummyMediaElement.canPlayType) {
+    console.log("canPlayType is not supported");
+    return false;
+  }
+
+  return !!dummyMediaElement.canPlayType(mimeType);
+}
+
+/**
+ * Validates the given file based on its size and type.
+ * @param {File} file - The file to validate.
+ * @returns {object} - An object with the validation result and error message (if any).
+ */
 export function validateFile(file) {
   if (file.size > UPLOAD_FILE_SIZE_LIMIT) {
     return {
@@ -26,9 +73,8 @@ export function validateFile(file) {
     };
   }
 
-  const isTypeSupported = ["image", "audio", "video"].some((t) =>
-    file.type.includes(t)
-  );
+  const isImage = file.type.startsWith("image");
+  const isTypeSupported = isImage || canPlayType(file.type);
 
   if (!isTypeSupported) {
     return {
@@ -40,6 +86,11 @@ export function validateFile(file) {
   return { valid: true, error: "" };
 }
 
+/**
+ * Gets the base64 URL of a B file.
+ * @param {object} bFile - The B file object containing the base64 data and content type.
+ * @returns {string|null} - The base64 URL of the file, or null if the file object is invalid.
+ */
 export function getBase64Url(bFile) {
   if (!bFile || !bFile.Data || !bFile["content-type"]) {
     return null;
