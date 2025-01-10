aab1eebf343e12ad35cb81817f5cb55d834a662d
Satchmo <rohenaz@gmail.com>
2025-01-09 20:58:09 -0500

    we need prefixes

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,15 +1,14 @@
 /// <reference types="vite/client" />
-import Bun from 'bun';
 
-export const API_BASE_URL = Bun.env.API_URL || 'http://localhost:3055';
+export const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3055';
 
 export const API_ENDPOINT = `${API_BASE_URL}`;
 
-export const HANDCASH_APP_ID = Bun.env.HANDCASH_APP_ID;
+export const HANDCASH_APP_ID = import.meta.env.VITE_HANDCASH_APP_ID;
 
-export const YOURS_APP_ID = Bun.env.YOURS_APP_ID;
+export const YOURS_APP_ID = import.meta.env.VITE_YOURS_APP_ID;
 
-export const ENVIRONMENT = Bun.env.NODE_ENV || 'development';
+export const ENVIRONMENT = import.meta.env.MODE || 'development';
 
 export const HANDCASH_API_URL =
-  Bun.env.HANDCASH_API_URL || 'https://api.bitchatnitro.com';
+  import.meta.env.VITE_HANDCASH_API_URL || 'https://api.bitchatnitro.com';
