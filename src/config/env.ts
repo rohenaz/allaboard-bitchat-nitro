a18b5d70f9c8f5f1dbbf1906e97619080f54d596
Satchmo <rohenaz@gmail.com>
2025-01-09 20:55:58 -0500

    no prefixes

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,14 +1,15 @@
 /// <reference types="vite/client" />
+import Bun from 'bun';
 
-export const API_BASE_URL = process.env.VITE_API_URL || 'http://localhost:3055';
+export const API_BASE_URL = Bun.env.API_URL || 'http://localhost:3055';
 
 export const API_ENDPOINT = `${API_BASE_URL}`;
 
-export const HANDCASH_APP_ID = process.env.VITE_HANDCASH_APP_ID;
+export const HANDCASH_APP_ID = Bun.env.HANDCASH_APP_ID;
 
-export const YOURS_APP_ID = process.env.VITE_YOURS_APP_ID;
+export const YOURS_APP_ID = Bun.env.YOURS_APP_ID;
 
-export const ENVIRONMENT = process.env.NODE_ENV || 'development';
+export const ENVIRONMENT = Bun.env.NODE_ENV || 'development';
 
 export const HANDCASH_API_URL =
-  process.env.VITE_HANDCASH_API_URL || 'https://api.bitchatnitro.com';
+  Bun.env.HANDCASH_API_URL || 'https://api.bitchatnitro.com';
