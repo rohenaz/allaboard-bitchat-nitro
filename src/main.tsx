24e54ba5b371912ab8a0b76c41e02dc7b96ac943
Luke Rohenaz <rohenaz@gmail.com>
2025-10-26 17:22:26 -0400

    Fix Yours wallet auto-connecting on page load
    - Change AutoYoursProvider autoconnect prop from true to false in main.tsx
    - Yours wallet will now only connect when user clicks 'Login with Yours Wallet'
    - Prevents unwanted popup on initial page load

--- src/main.tsx
+++ src/main.tsx
@@ -20,7 +20,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <YoursProvider>
-        <AutoYoursProvider autoconnect={true}>
+        <AutoYoursProvider autoconnect={false}>
           <HandcashProvider>
             <BmapProvider>
               <BapProvider>
